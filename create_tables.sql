fa11b202c023f0e57a4ac4194936c5e409dca8b8
Eric Peers <peers@mtnboy.net>
2026-02-24 18:41:52 -0700

    cleanup lint errors, remove 'drop tables' that don't exist. Capitalization fix for types. Add index on ticker to make faster lookups

--- create_tables.sql
+++ create_tables.sql
@@ -6,29 +6,27 @@
 
 -- assumption is this will go into a postgres DB called securities
 -- createdb securities
+drop index if exists idx_ds_ticker;
 drop table if exists dim_exchanges cascade;
 drop table if exists dim_security cascade;
-drop table if exists dim_stock_index cascade;
-drop table if exists dim_stock_index_membership cascade;
-drop table if exists dim_etf cascade;
 drop table if exists dim_etf_membership cascade;
 drop table if exists dim_etf_pull_range cascade;
 drop table if exists dim_user cascade;
 
 drop table if exists portfolio cascade;
 drop table if exists portfolio_membership cascade;
-drop type if exists PF_TYPE;
-drop type if exists PF_OBJECTIVE;
-drop type if exists DS_TYPE;
+drop type if exists pf_type;
+drop type if exists pf_objective;
+drop type if exists ds_type;
 
 drop table if exists fact_price_range cascade;
 drop table if exists fact_price;
 drop table if exists fact_event;
 
 create table dim_exchanges (
-    id SERIAL primary key,
-    name VARCHAR(80),
-    country VARCHAR(80)
+    id serial primary key,
+    name varchar(80),
+    country varchar(80)
 );
 
 insert into dim_exchanges (name, country) values
@@ -42,9 +40,8 @@ create type ds_type as enum (
     'BOND',  -- normally a loan to the US/other gvmt like US10Y which is the US 10 year treasury or "risk free rate"
     'ETC',    -- Exchange traded commodity like silver or gold
     'ETF',    -- group of stocks that don't incur taxes on redemption when buy/sell for individual - goes just to the person who sold. Tax advantaged
-    'FUND',  -- Money markets, 
+    'FUND',  -- Money markets, Mutual Funds : mixture of securities, where the fund buys/sells on redemption from larger pool. Tax disadvantaged
     'INDEX', -- Top X stocks in a certain exchange or country like the DOW
-    'MUTUAL FUND',  -- mixture of securities, where the fund buys/sells on redemption from larger pool. Tax disadvantaged
     'NOTES',   -- debt, traded as a grouping like an ETF. Also called ETN
     'UNIT',    -- mixture of stock and warrants
     'WARRANT', -- right to buy shares at a price, l ike an option
@@ -54,20 +51,22 @@ create type ds_type as enum (
 );
 
 create table dim_security (
-    id BIGSERIAL primary key,
-    ticker VARCHAR(30), --NXT(EXP20091224) is potentially a bad security. Some securities are long like ASRV 8.45 06-30-28. Morocco has 30 character tickers. Sigh. 
-    name VARCHAR(200),
-    isin VARCHAR(12), -- worldwide code to identify the stock.
-    exchange SERIAL references dim_exchanges (id),
-    currency VARCHAR(3),
+    id bigserial primary key,
+    ticker varchar(30), --NXT(EXP20091224) is potentially a bad security. Some securities are long like ASRV 8.45 06-30-28. Morocco has 30 character tickers. Sigh. 
+    name varchar(200),
+    isin varchar(12), -- worldwide code to identify the stock.
+    exchange serial references dim_exchanges (id),
+    currency varchar(3),
     -- TODO FIXME. Consider adding sector
     -- sector VARCHAR(30),
-    inception DATE,
-    url VARCHAR, --useful for holdings on mutual funds, etf, reit, index.
-    type DS_TYPE not null,
+    inception date,
+    url varchar, --useful for holdings on mutual funds, etf, reit, index.
+    type ds_type not null,
 
     constraint only_one_ticker_per_exchange unique (ticker, exchange)
 );
+create index idx_ds_ticker on dim_security (ticker);
+
 -- FIXME. HACK XXX. Remove
 -- This is a manually inserted test security for bootstrapping.
 insert into dim_security (
@@ -97,26 +96,26 @@ insert into dim_security (
 
 -- tracks when ETF membership data was fetched (one row per ETF, not per member)
 create table dim_etf_pull_range (
-    composite_id BIGSERIAL references dim_security (id),
-    pull_date DATE,
-    next_update TIMESTAMPTZ,
+    composite_id bigserial references dim_security (id),
+    pull_date date,
+    next_update timestamptz,
     primary key (composite_id)
 );
 
 create table dim_etf_membership (
-    dim_security_id BIGSERIAL references dim_security (id), -- this is the member like NVDA
-    dim_composite_id BIGSERIAL references dim_security (id), -- this is the parent like SPY
-    percentage FLOAT,
+    dim_security_id bigserial references dim_security (id), -- this is the member like NVDA
+    dim_composite_id bigserial references dim_security (id), -- this is the parent like SPY
+    percentage float,
 
     primary key (dim_security_id, dim_composite_id)
 );
 
 create table dim_user (
-    id BIGSERIAL primary key,
-    name VARCHAR(256),
-    email VARCHAR(256), -- https://www.rfc-editor.org/errata_search.php?rfc=3696
-    passwd VARCHAR(256), -- one way hash this.
-    join_date DATE
+    id bigserial primary key,
+    name varchar(256),
+    email varchar(256), -- https://www.rfc-editor.org/errata_search.php?rfc=3696
+    passwd varchar(256), -- one way hash this.
+    join_date date
 );
 
 -- FIXME - we probably want this inserted via API call, not manually injected here.
@@ -135,22 +134,22 @@ create type pf_type as enum ('Ideal', 'Active', 'Historic');
 -- these are like FACT tables since they may be edited. It's not a one time add.
 -- they are also like DIM tables since they don't have a bunch of data.
 create table portfolio (
-    id BIGSERIAL primary key,
-    portfolio_type PF_TYPE,
-    objective PF_OBJECTIVE not null,
-    name VARCHAR(80),
-    comment TEXT,   -- additional comments about this portfolio
-    created DATE,
+    id bigserial primary key,
+    portfolio_type pf_type,
+    objective pf_objective not null,
+    name varchar(80),
+    comment text,   -- additional comments about this portfolio
+    created date,
     -- used for historic portfolios - where you might change them
-    ended DATE,
-    updated DATE,
-    owner BIGSERIAL references dim_user (id)
+    ended date,
+    updated date,
+    owner bigserial references dim_user (id)
 );
 
 create table portfolio_membership (
-    portfolio_id BIGSERIAL references portfolio (id),
-    security_id BIGSERIAL references dim_security (id),
-    percentage_or_shares FLOAT,
+    portfolio_id bigserial references portfolio (id),
+    security_id bigserial references dim_security (id),
+    percentage_or_shares float,
 
     primary key (portfolio_id, security_id)
 );
@@ -175,21 +174,21 @@ create table portfolio_membership (
 --    it is possible to simplify this (at the cost of holiday refetch-es) to if before 4:15pm on a business day, then wait. If after 4:15 or non business day, use next day.
 --
 create table fact_price_range (
-    security_id BIGSERIAL references dim_security (id),
-    start_date DATE,
-    end_date DATE,
-    next_update TIMESTAMPTZ,
+    security_id bigserial references dim_security (id),
+    start_date date,
+    end_date date,
+    next_update timestamptz,
     primary key (security_id)
 );
 
 create table fact_price (
-    security_id BIGSERIAL references dim_security (id),
-    date DATE,
-    open FLOAT,
-    high FLOAT,
-    low FLOAT,
-    close FLOAT,
-    volume FLOAT,
+    security_id bigserial references dim_security (id),
+    date date,
+    open float,
+    high float,
+    low float,
+    close float,
+    volume float,
     primary key (security_id, date)
 );
 
@@ -197,10 +196,10 @@ create table fact_price (
 -- since that data is generally sparse (0 for dividend, and 1.0 for split), put it in another table
 -- for quick lookup
 create table fact_event (
-    security_id BIGSERIAL references dim_security (id),
-    date DATE,
+    security_id bigserial references dim_security (id),
+    date date,
 
-    dividend FLOAT,
-    split_coefficient FLOAT,
+    dividend float,
+    split_coefficient float,
     primary key (security_id, date)
 );
